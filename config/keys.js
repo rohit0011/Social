c7de136fc213444bff27a11c4b8301493631f9c0
1606214 <1606214@kiit.ac.in>
2018-07-27 23:45:19 +0530

    make it better

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://rohit:akSH125!@ds235431.mlab.com:35431/storybook-rohit',
+  googleClientID:'430925584376-jm8ufculkds7f004cpmluteri2uq4k01.apps.googleusercontent.com',
+  googleClientSecret:'cXrFXJ19-GRq3iC4S2-KVV2K'
+}
